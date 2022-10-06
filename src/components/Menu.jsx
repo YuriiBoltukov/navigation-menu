d803e54ead17bb5508b06118f087a392f543c067
YuriiBoltukov <kolomiha898@gmail.com>
2022-10-06 14:04:42 +0300

    feat homework

---
+++ src/components/Menu.jsx
@@ -0,0 +1,20 @@
+import { Link, NavLink } from 'react-router-dom';
+
+export default function Menu() {
+	return (
+		<nav className='menu'>
+			<Link to='/' className='menu__item'>
+				Главная
+			</Link>
+			<Link to='/drift' className='menu__item'>
+				Дрифт-такси
+			</Link>
+			<Link to='/timeattack' className='menu__item'>
+				Time Attack
+			</Link>
+			<Link to='/forza' className='menu__item'>
+				Forza Karting
+			</Link>
+		</nav>
+	);
+}
